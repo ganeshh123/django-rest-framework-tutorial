16afec8f1f35a28fa6433756e1baecbe2e1bb866
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2026-05-24 13:05:48 +0530

    02-create-serializers-and-api-views-for-products

--- drf_tutorial/urls.py
+++ drf_tutorial/urls.py
@@ -1,3 +1,5 @@
+from django.urls import include, path
+
 """
 URL configuration for drf_tutorial project.
 
@@ -19,4 +21,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('api.urls'))
 ]
