396790979a901b70544903a9e5fc182a756a6cd4
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2026-05-24 13:50:07 +0530

    03-nested-serializers, serializermethodfield, serializer relations

--- api/serializers.py
+++ api/serializers.py
@@ -18,3 +18,51 @@ class ProductSerializer(serializers.ModelSerializer):
                 "Prce must be greater than 0."
             )
         return value
+    
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(
+        source='product.price',
+        max_digits=10,
+        decimal_places=2,
+        coerce_to_string=False
+    )
+    product_subtotal = serializers.DecimalField(
+        source='item_subtotal',
+        max_digits=10,
+        decimal_places=2,
+        coerce_to_string=False
+    )
+    product_detail = serializers.SerializerMethodField()
+    
+    def get_product_detail(self, obj):
+        product = obj.product
+        return f'/products/{product.id}'
+    
+    class Meta:
+        model = OrderItem
+        fields = ('product_name', 'product_detail', 'product_price','quantity', 'product_subtotal')
+    
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
+    
+    def get_user(self, obj):
+        user = obj.user
+        return f"[{user.id}] {user.username}"
+    
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+    
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'created_at',
+            'user',
+            'status',
+            'items',
+            'total_price'
+        )
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer
+from api.models import Product, Order, OrderItem
 
 @api_view(['GET'])
 def product_list(request):
@@ -14,4 +14,10 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
