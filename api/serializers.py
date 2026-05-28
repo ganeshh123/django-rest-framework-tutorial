116b817ecf91eba2b3d9a25545b3e80a1557a74e
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2026-05-28 11:52:01 +0700

    chore: ruff formatting

--- api/serializers.py
+++ api/serializers.py
@@ -56,9 +56,8 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ("order_id", "created_at", "user", "status", "items", "total_price")
 
+
 class ProductInfoSerializer(serializers.Serializer):
     products = ProductSerializer(many=True)
     count = serializers.IntegerField()
     max_price = serializers.FloatField()
-    
-    
