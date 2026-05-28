6aa610f4873ba52b9d24d03acce10a7a6f9b616c
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2026-05-28 11:29:24 +0700

    chore: add and configure ruff linting

--- drf_tutorial/urls.py
+++ drf_tutorial/urls.py
@@ -19,7 +19,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls'))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("api.urls"))]
