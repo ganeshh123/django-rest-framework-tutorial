576fb149c8d09e458dd9cfea8c3ad5f6ccde7082
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2026-05-28 12:30:16 +0700

    07-dynamic-filtering, get_queryset

--- api/views.py
+++ api/views.py
@@ -37,6 +37,15 @@ class OrderListAPIView(generics.ListAPIView):
     serializer_class = OrderSerializer
 
 
+class UserOrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related("items__product", "user").all()
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
+
+
 # @api_view(["GET"])
 # def order_list(request):
 #     orders = Order.objects.prefetch_related("items__product", "user").all()
