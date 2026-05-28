bae70167e015ef478b7ba6fa5e215fa960a0c724
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2026-05-28 11:52:35 +0700

    05-django-silk profiling and prefetch optimization

--- drf_tutorial/urls.py
+++ drf_tutorial/urls.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.urls import include, path
 
-from django.contrib import admin
-
-urlpatterns = [path("admin/", admin.site.urls), path("", include("api.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("api.urls")),
+    path("silk/", include("silk.urls", namespace="silk")),
+]
